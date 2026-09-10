a30cc3c22b8c758c575fb0c7aec6e44c6477089c
gabrielmartielo <gabrielmartielo3214@gmail.com>
2026-09-10 20:39:32 -0300

    add: implement geometric figure classes with area calculation methods

---
+++ aula10-09/run2.py
@@ -0,0 +1,39 @@
+from abc import ABC, abstractmethod
+
+class figuraGeometrica(ABC):
+    def __init__(self, nome):
+        self.nome = nome
+
+    @abstractmethod
+    def calcular_area(self):
+        pass
+
+class Retangulo(figuraGeometrica):
+    def __init__(self, nome, base, altura):
+        super().__init__('Retângulo')
+        self.base = base
+        self.altura = altura
+
+    def calcular_area(self):
+        return self.base * self.altura
+
+class Circulo(figuraGeometrica):
+    def __init__(self, nome, raio):
+        super().__init__('Círculo')
+        self.raio = raio
+
+    def calcular_area(self):
+        return 3.14 * self.raio ** 2
+
+class Triangulo(figuraGeometrica):
+    def __init__(self, nome, base, altura):
+        super().__init__('Triângulo')
+        self.base = base
+        self.altura = altura
+
+    def calcular_area(self):
+        return self.base * self.altura    
+
+figura = figuraGeometrica(
+    nome='Retângulo'
+)
