6131d4bb28d30f8bac8d505230e2eec1ad74a0d7
gabrielmartielo <dev.gabrielmartielo@gmail.com>
2026-09-16 14:47:06 -0300

    add: implement execution logging for email sending and report generation functions

--- exercicios/ex3/run.py
+++ exercicios/ex3/run.py
@@ -1,3 +1,31 @@
-from abc import ABC, abstractmethod
+from functools import wraps
 
 
+def registrar_execucao(funcao):
+    @wraps(funcao)
+    def wrapper(*args, **kwargs):
+        print(f"Iniciando execução da função: {funcao.__name__}...")
+
+        resultado = funcao(*args, **kwargs)
+
+        print("Execução finalizada.")
+        return resultado
+
+    return wrapper
+
+
+@registrar_execucao
+def enviar_email(destinatario):
+    print(f"E-mail enviado para {destinatario}")
+
+
+@registrar_execucao
+def gerar_relatorio(nome):
+    print(f"Relatório '{nome}' foi gerado.")
+
+
+enviar_email("cliente@email.com")
+
+print()
+
+gerar_relatorio("Relatório de Vendas")
